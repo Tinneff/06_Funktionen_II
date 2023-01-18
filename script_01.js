92f0d30be7019f5fd47b9f7de5ad16329338de26
Peter <schmidt.multimedia@gmail.com>
2023-01-18 10:49:55 +0100

    module: addition a + b

--- script_01.js
+++ script_01.js
@@ -13,8 +13,16 @@
 
 
 
+// module: addition a + b |  test:
+output(add(2,2));
+output(add(2,-2));
+output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
 
-// module:  output | test:
+
+// module: output | test:
 // output("hello");
 // output(2);
 function output(outputData) {
