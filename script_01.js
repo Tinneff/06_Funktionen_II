8e9ac784cd2ea3e7b38785bd1c15a78049ee0ad6
Peter <schmidt.multimedia@gmail.com>
2023-01-18 10:57:34 +0100

    module: subtraction a - b

--- script_01.js
+++ script_01.js
@@ -11,16 +11,25 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: multiplication a * b |  test:
 
+
+
+// module: subtraction a - b |  test:
+output(subtract(3,2));
+output(subtract(3,-2));
+output(subtract(3,0));
+function subtract(a,b) {
+	return a - b;
+}
 
 // module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
 function add(a,b) {
 	return a + b;
 }
-
 
 // module: output | test:
 // output("hello");
