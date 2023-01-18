45dd0e90c56a86ca903186b4d0c8c8c6e3dd9234
Tinneff <tadeus@unitybox.de>
2023-01-18 12:32:20 +0100

    Version 1 ++

--- script_01.js
+++ script_01.js
@@ -1,20 +1,82 @@
-
 "use strict";
 
 /*** Rechner */
 /*
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
-
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart :
-3. Fkt. Grundrechenarten :
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
 
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
 
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
 
-// module:  output | test:
+function calculator(a,b,op) {
+	switch (op) {
+		case "+": // addition
+			return "Ergebnis addition";
+		case "-": // subtraktion
+			return "Ergebnis subtraktion";
+		case "*": // multiplikation
+			return "Ergebnis multiplikation";
+		case ":": // divison
+		case "/": // divison
+			return "Ergebnis divison";
+		default:
+			return ERROR_STR_GEN;
+	}
+}
+
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
+function divide(a,b) {
+	if (b == 0) { 
+		return ERROR_STR_DIV;
+	} 
+	return a / b;
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+    return a * b; 
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+function subtract(a,b) {
+	return a - b;
+}
+
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
+
+// module: output | test:
 // output("hello");
 // output(2);
 function output(outputData) {
