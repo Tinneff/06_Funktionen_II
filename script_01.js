3abce15baddffac944145bf565d245d150cf4628
Tinneff <tadeus@unitybox.de>
2023-01-19 09:17:46 +0100

    version 4 ++

--- script_01.js
+++ script_01.js
@@ -4,7 +4,7 @@
 /*
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart :
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
@@ -13,28 +13,58 @@
 const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 const ERROR_STR_GEN = "Irgendetwas ging schief!"
 
+// startApp();
+function startApp() {
+	output(calculator(getNumber1(),getNumber2(),getOp()));
+}
+
+function getNumber1() {
+	return parseInt(prompt("Zahl 1 eingeben:"));
+}
+
+function getNumber2() {
+	return parseInt(prompt("Zahl 2 eingeben:"));
+}
+
+// module: operator input| Test:
+output(getOp());
+function getOp() {
+
+	let op = prompt("Operator eingeben:");
+	
+	if (op == "+" || op == "-" || op == "*" || op == "/" || op == ":") {
+
+		return op;
+	
+	} else {
+	
+		return ERROR_STR_GEN;
+	
+	}
+
+}
+
+
 // module: calculator | tests:
 // agreement : "+","-","*",":","/"
-
-output(calculator(3,2,"+"));
-output(calculator(3,2,"-"));
-output(calculator(3,2,"*"));
-output(calculator(3,2,":"));
-output(calculator(3,2,"/"));
-output(calculator(3,0,"/"));
-output(calculator(3,2,"#?!"));
-
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 function calculator(a,b,op) {
 	switch (op) {
 		case "+": // addition
-			return "Ergebnis addition";
+			return add(a,b);
 		case "-": // subtraktion
-			return "Ergebnis subtraktion";
+			return subtract(a,b);
 		case "*": // multiplikation
-			return "Ergebnis multiplikation";
+			return multiply(a,b);
 		case ":": // divison
 		case "/": // divison
-			return "Ergebnis divison";
+			return divide(a,b);
 		default:
 			return ERROR_STR_GEN;
 	}
